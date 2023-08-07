bf8ec5427256da59ac8686f22e81feb282910d8a
rcarmas <rcarmas@espe.edu.ec>
2023-08-07 10:53:46 -0500

    Update III

--- taller_res_partner/models/taller_res_partner.py
+++ taller_res_partner/models/taller_res_partner.py
@@ -2,4 +2,4 @@ from odoo import models, fields, api, _
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
-    ingreso=fields.Char(string="Ingreso")
+    ingreso = fields.Char(string="Ingreso")
